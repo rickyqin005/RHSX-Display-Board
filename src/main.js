882009b0e066d83a806472f00058a7c7f703d626
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-09-14 22:12:02 -0400

    added text-table

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ const { Client } = require('discord.js');
 const { GatewayIntentBits } = require('discord-api-types/v10');
 const discordClient = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 discordClient.on('debug', console.log);
+// Axios
+const axios = require('axios');
+const URL = 'https://rhsx.rickyqin.repl.co/api/display_board';
+
 let message;
 const REFRESH_RATE = 6000;
 const Price = require('./utils/Price');
 const Tools = require('./utils/Tools');
-// Axios
-const axios = require('axios');
-const URL = 'https://rhsx.rickyqin.repl.co/api/display_board';
+const createTable = require('text-table');
 
 async function update() {
     const startTime = new Date();
@@ -25,33 +27,30 @@ async function update() {
 
 function displayBoardString(data) {
     let str = `Last updated at ${Tools.dateStr(new Date())}\n`;
-    str += '```\n';
-    str += Tools.setW('Ticker', 8) + Tools.setW('Price', 10) + Tools.setW('Bid', 10) + Tools.setW('Ask', 10) + Tools.setW('Volume', 10) + '\n';
+    const mainTable = [ ['Ticker', 'Last', 'Bid', 'Ask', 'Volume'] ];
     for(const symbol in data.tickers) {
         const ticker = data.tickers[symbol];
         const topBid = (ticker.bids.length > 0 ? ticker.bids[0].price : undefined);
         const topAsk = (ticker.asks.length > 0 ? ticker.asks[0].price : undefined);
-        str += Tools.setW(symbol, 8) + Tools.setW(Price.format(ticker.lastTradedPrice), 10) +
-        Tools.setW(Price.format(topBid), 10) + Tools.setW(Price.format(topAsk), 10) + Tools.setW(ticker.volume, 10) + '\n';
+        mainTable.push([symbol, Price.format(ticker.lastTradedPrice), Price.format(topBid), Price.format(topAsk), ticker.volume]);
     }
-    str += '```\n\n';
+    str += '```\n' + createTable(mainTable) + '```\n\n';
     for(const symbol in data.tickers) {
         const ticker = data.tickers[symbol];
         str += `Ticker: **${symbol}**\n`;
-        str += '```\n';
-        str += Tools.setW('Bids', 16) + 'Asks' + '\n';
+        const orderBookTable = [ ['Bids', 'Asks'] ];
         for(let i = 0; i < Math.max(ticker.bids.length, ticker.asks.length); i++) {
+            orderBookTable.push(['', '']);
             if(i < ticker.bids.length) {
                 const bid = ticker.bids[i];
-                str += Tools.setW(`@${Price.format(bid.price)} x${bid.quantity}`, 16);
-            } else str += Tools.setW('', 16);
+                orderBookTable[i+1][0] = `@${Price.format(bid.price)} x${bid.quantity}`;
+            }
             if(i < ticker.asks.length) {
                 const ask = ticker.asks[i];
-                str += `@${Price.format(ask.price)} x${ask.quantity}`;
+                orderBookTable[i+1][1] = `@${Price.format(ask.price)} x${ask.quantity}`;
             }
-            str += '\n';
         }
-        str += '```\n';
+        str += '```\n' + createTable(orderBookTable) + '```\n';
     }
     return str;
 }
